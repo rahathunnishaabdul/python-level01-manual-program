4899020e9abfac243596b909cecf34550f22ac96
rahathunnishaabdul <rahathunnisha24@gmail.com>
2025-08-04 10:02:57 +0530

    py3

---
+++ py3.py
@@ -0,0 +1,11 @@
+print("government of tamilnadu")
+print("electricity board")
+print("------------------")
+str1=input("enter the eb no:")
+str2=input("enter the customber name:")
+no1=int(input("reading for previous month"))
+no2=int(input("reading for current month:"))
+total=no2-no1
+print("total unit comsumed:",total)
+paid=total*5
+print("amount to be paid rs:",paid)
