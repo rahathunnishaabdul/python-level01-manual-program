e03e2d10254b3ccfc2f5bb0cf2b091169b5aa552
rahathunnishaabdul <rahathunnisha24@gmail.com>
2025-08-04 09:51:54 +0530

    py2

---
+++ nisha2.py
@@ -0,0 +1,14 @@
+print("Takshashila university")
+print("ongur,tindivanam,villupuram dist.TN")
+print("------------------------------------")
+print("student mark list")
+print("------------------")
+str1=input("Enter the register no:")
+str2=input("Enter the name:")
+m1=int(input("python mark:"))
+m2=int(input("DBMS mark:"))
+m3=int(input("Accounting mark:")) 
+total=m1+m2+m3
+print=("Total mark:",total)
+average=total/3
+print("Average mark:",average)
