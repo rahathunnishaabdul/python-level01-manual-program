0bfcfa5cb35faf63cf5bdafd3bab09e8949f05f5
rahathunnishaabdul <rahathunnisha24@gmail.com>
2025-08-11 10:19:48 +0530

    py10

---
+++ py10.py
@@ -0,0 +1,14 @@
+print("Exponential program:")
+print("-------------------:")
+x=int(input("Enter the x value:"))
+y=int(input("Enter the y value:"))
+z=int(input("Enter the z value:"))
+print("Result:")
+P=(x*x)+(y*y)+(z*z)
+print("P:",P)
+Q=(x*x*x)+(y*y)+z+20
+print("Q:",Q)
+R=(x*x*y)+(y*y*z)+(z*z*x)+10
+print("R:",R)
+S=(x*x*x*y*y*z)+2(x*y*z)+x*y*z
+print("S:",S)
