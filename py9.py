bc4bf45641dab5c8e2a6bee2ce66a53a70cd71b2
rahathunnishaabdul <rahathunnisha24@gmail.com>
2025-08-11 10:07:14 +0530

    py9

---
+++ py9.py
@@ -0,0 +1,10 @@
+print("sum of N numbers")
+print("-----------------")
+n=int(input("Enter the n value:"))
+print("Result")
+sum=n*(n+1)/2
+print("sum:",sum)
+s.sq=(n*(n+1)*(2*n+1))/6
+print("s.sq:",s.sq)
+s.c=(n*(n+1)/2)*(n*(n+1)/2)
+print("s.c:",s.c)
