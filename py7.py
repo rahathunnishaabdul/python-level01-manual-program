848accc086f29ffedea7233c2acbe9dae82621fe
rahathunnishaabdul <rahathunnisha24@gmail.com>
2025-08-11 10:44:38 +0530

    py7

---
+++ py7.py
@@ -0,0 +1,8 @@
+print("circle program")
+print("--------------")
+r=int(input("Enter the R value:"))
+print("Result:")
+peri=2*22/7
+print("perimeter of circle:",peri)
+area=22/7*r*r
+print("area of cirle:",area)
